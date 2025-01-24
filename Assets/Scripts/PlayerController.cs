0699443f638ef5b5b6774931f8c171e6790fc129
rmleverton <49069613+rmleverton@users.noreply.github.com>
2025-01-24 20:00:16 +0000

    ingredient player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     InputAction moveAction;
+    InputAction interactAction;
     public float speed;
+    public string ingredient;
 
     private void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        interactAction = InputSystem.actions.FindAction("Interact");
     }
 
     void Update()
@@ -19,5 +22,7 @@ public class PlayerController : MonoBehaviour
         // to your GameObject
         this.transform.position = new Vector3(this.transform.position.x + moveValue.x, this.transform.position.y, this.transform.position.z + moveValue.y);
     }
+
+    
 
 }
